007a4fd58309d2e55b160909d0cdb6fd176625db
Fiorela Huarcaya <fiorela.16.febrero@gmail.com>
2022-12-20 00:24:07 -0500

    proxy

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+const fetch = (url) => import('node-fetch').then(({default: fetch}) => fetch(url));
+
+
 const express = require('express')
 const app = express()
 const port = 80
 const path = require('path');
 const router = express.Router();
 
+router.get('/:id',async function(req,res){
+  let id = req.params.id
+  await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+          .then(response => response.json())
+          .then(json => {
+                console.log(json)
+                res.send(json)
+              })
+});
 
-router.get('/',function(req,res){
+/*router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
@@ -13,7 +25,7 @@ router.get('/',function(req,res){
 router.get('/api', (req, res) => {
   //res.json({ username: 'Flavio' })
   res.send({ username: 'Fio Conera' })
-})
+})*/
 
 //add the router
 app.use('/', router);
