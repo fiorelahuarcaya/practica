9d0a8bbd5d112ed7baaae2d769a3d46eb39a9458
Fiorela Huarcaya <fiorela.16.febrero@gmail.com>
2022-12-19 23:42:43 -0500

    return html and json

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
+const path = require('path');
+const router = express.Router();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get('/api', (req, res) => {
+  //res.json({ username: 'Flavio' })
+  res.send({ username: 'Fio Conera' })
 })
 
+//add the router
+app.use('/', router);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
