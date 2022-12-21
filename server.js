b92439ef71df98d030b5d9bfc43046b2f5e6474c
Fiorela Huarcaya <fiorela.16.febrero@gmail.com>
2022-12-20 22:14:45 -0500

    post method

--- server.js
+++ server.js
@@ -7,9 +7,20 @@ const port = 80
 const path = require('path');
 const router = express.Router();
 
-router.get('/:id',async function(req,res){
+app.use(express.json());       // to support JSON-encoded bodies
+
+router.get('/api/:id',async function(req,res){
   let id = req.params.id
-  await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+  let response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+  let data= await response.json();
+  console.log(data)
+  res.send(data)
+});
+
+router.post('/api',async function(req,res){
+  let body = req.body;
+
+  await fetch(`https://jsonplaceholder.typicode.com/todos/${body.id}`)
           .then(response => response.json())
           .then(json => {
                 console.log(json)
@@ -17,7 +28,7 @@ router.get('/:id',async function(req,res){
               })
 });
 
-/*router.get('/',function(req,res){
+router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
@@ -25,7 +36,7 @@ router.get('/:id',async function(req,res){
 router.get('/api', (req, res) => {
   //res.json({ username: 'Flavio' })
   res.send({ username: 'Fio Conera' })
-})*/
+})
 
 //add the router
 app.use('/', router);
